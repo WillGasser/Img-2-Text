0c90a93ecbb34875bc488393d315fe0d705bea90
Will Gasser <willbg04@gmail.com>
2024-11-10 22:48:22 -0600

    nill change

--- main.cpp
+++ main.cpp
@@ -1,28 +1,46 @@
-// gcc -o run.exe main.c
+// g++ -o run.exe main.c
 
 #include <iostream>
 #include <vector>
 #include <string>
 #include <fstream>
+#include <stdio.h>
+
+#define STB_IMAGE_IMPLEMENTATION
+#include "stb_image.h"
+
+#include <filesystem>
+
 using namespace std;
 
-int main()
+void readFiles(string directory)
 {
     FILE *img = fopen("img0.png", "rb");
-
     if (img == nullptr)
     {
         cout << "Error opening file" << endl;
-        return 1;
     }
-
     string filecontents = "";
     int c = 0;
     while ((c = fgetc(img)) != EOF)
     {
         filecontents += static_cast<char>(c);
     }
-
     fclose(img);
     printf("%s", filecontents.c_str());
+}
+
+void displayFile(string fileName)
+{
+    FILE *image = fopen(fileName, "rb");
+
+    if (image == nullptr)
+    {
+        cout << "Error opening file" << endl;
+    }
+}
+
+int main()
+{
+    displayFile("testImages/img0.png") return 0;
 }
